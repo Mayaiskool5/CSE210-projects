c1446ac002506fdab0fd71564def31a2ed1b76fc
Use your GitHub username <jer14002@byui.edu>
2025-09-25 12:59:51 -0400

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //For Part 1 and 2, where the user specified the number...
+        //Console.Write("What is the magic number? ");
+        //int magicNum = int.Parse(Console.ReadLine());
+
+        //For Part 3, where we use a random number
+        Random randomGen = new Random();
+        int magicNum = randomGen.Next(1, 101);
+        
+        int guess = -1;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed correctly!");
+            }
+        }
     }
 }
