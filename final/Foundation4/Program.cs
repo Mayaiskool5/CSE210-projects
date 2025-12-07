3a34dbf7f84336086e0bea52530d826f9de52854
Use your GitHub username <jer14002@byui.edu>
2025-12-07 11:02:39 -0500

    updates to Foundation4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         List<Activity> activities = new List<Activity>();
 
-        Running running = new Running("6/3/2024", 30, "Running", 5);
-        Biking biking = new Biking("6/7/2024", 40, "Biking", 30);
-        Swimming swimming = new Swimming("6/10/2024", 20, "Swimming", 5);
+        Running running = new Running("6/3/2024", 40, "Running", 10);
+        Biking biking = new Biking("6/7/2024", 60, "Biking", 40);
+        Swimming swimming = new Swimming("6/10/2024", 30, "Swimming", 6);
 
         activities.Add(running);
         activities.Add(biking);
