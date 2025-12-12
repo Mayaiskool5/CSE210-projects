cea9a0e89c22c5dc15ab5893495df1be483ffd01
Use your GitHub username <jer14002@byui.edu>
2025-12-11 20:03:36 -0500

    added

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,24 +1,24 @@
 public class Video
 {
-    private string title;
-    private string youtuber;
-    private int length; // Length of video
+    private string _title;
+    private string _youtuber;
+    private int _length; // Length of video
 
     public void getLength(int minutes, int seconds)
     {
-        length = (minutes * 60) + seconds;
+        _length = (minutes * 60) + seconds;
     }
     public void getTitle(string Title)
     {
-        title = Title;
+        _title = Title;
     }
     public void getYoutuber(string Youtuber)
     {
-        youtuber = Youtuber;
+        _youtuber = Youtuber;
     }
     public string returnInfo()
     {
-        return $"Title: {title}\n Youtuber: {youtuber}\n Length: {length} seconds \n Comments:";
+        return $"Title: {_title}\n Youtuber: {_youtuber}\n Length: {_length} seconds \n Comments:";
     }
 
 }
